76437ff58009063c059d4e2e94ccda9151e86317
Michael Leone <mleone896@gmail.com>
2026-01-21 13:03:02 -0500

    feat: add ctrlc api get commands (#5) * add environments list * add systems list * add deployments list * removed unused query params * renamed file and systems command to be spelled correctly

---
+++ cmd/ctrlc/root/api/get/systems/systems.go
@@ -0,0 +1,59 @@
+package systems
+
+import (
+	"fmt"
+
+	"github.com/ctrlplanedev/cli/internal/api"
+	"github.com/ctrlplanedev/cli/internal/cliutil"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func NewSystemsCmd() *cobra.Command {
+	var limit int
+	var offset int
+
+	cmd := &cobra.Command{
+		Use:   "systems",
+		Short: "Get systems",
+		Long:  `Commands for getting systems.`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			apiURL := viper.GetString("url")
+			apiKey := viper.GetString("api-key")
+			workspace := viper.GetString("workspace")
+
+			client, err := api.NewAPIKeyClientWithResponses(apiURL, apiKey)
+			if err != nil {
+				return fmt.Errorf("failed to create API client: %w", err)
+			}
+
+			workspaceID := client.GetWorkspaceID(cmd.Context(), workspace)
+
+			if workspaceID.String() == "00000000-0000-0000-0000-000000000000" {
+				return fmt.Errorf("invalid workspace: %s", workspace)
+			}
+
+			params := &api.ListSystemsParams{}
+			if limit > 0 {
+				params.Limit = &limit
+			}
+			if offset > 0 {
+				params.Offset = &offset
+			}
+
+			resp, err := client.ListSystems(cmd.Context(), workspaceID.String(), params)
+			if err != nil {
+				return fmt.Errorf("failed to get resources: %w", err)
+			}
+
+			return cliutil.HandleResponseOutput(cmd, resp)
+		},
+	}
+
+	cmd.Flags().IntVarP(&limit, "limit", "l", 50, "Limit the number of results")
+	cmd.Flags().IntVarP(&offset, "offset", "o", 0, "Offset the results")
+
+	cmd.MarkFlagRequired("workspace")
+
+	return cmd
+}
