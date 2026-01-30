0863f5de48afdc1f306d60227276a7414479546c
Daniel Barnes <dabarnes2b@gmail.com>
2026-01-31 04:35:21 +0900

    chore: remove unused dep

--- cmd/ctrlc/root/sync/aws/networks/networks.go
+++ cmd/ctrlc/root/sync/aws/networks/networks.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/MakeNowJust/heredoc/v2"
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/charmbracelet/log"
